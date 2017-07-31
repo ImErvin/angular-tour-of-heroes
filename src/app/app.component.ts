6ab0fc04ad94d3977e617fa07d26c6bf59ee714d
Ervin Mamutov <erviin@live.ie>
2017-07-31 13:40:23 +0100

    Added input box and used ngModel(Import FormsModule) to bind hero.name to the input box

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  template:"<h1>{{title}}</h1><h2>{{hero.name}}'s details<h2>",
+  template: `<h1>{{title}}</h1>
+  <h2>{{hero.name}}'s details</h2>
+  <div><label>id: </label>{{hero.id}}</div>
+  <div>
+    <label>name: </label>
+    <input [(ngModel)]="hero.name" placeholder="name">
+  </div>`,
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'Tour of Heroes';
   hero: Hero = {
